37a6fc7846c912d465b13df88fdc32190c8e1511
OnlineElite <online.0.elite@gmail.com>
2023-04-20 02:57:23 +0000

    complate exercises

---
+++ Week-4/Day-1/Exercise-1/script.js
@@ -0,0 +1,9 @@
+
+/*------------------------Exercise-1--------------------*/
+function calcAge(myAge){
+    var mumAge = 2*myAge;
+    var dadAge = mumAge*1.2;
+    console.log("My mum age is: " + mumAge + " years old And my dad age is: "+ dadAge +" years old")
+}
+
+calcAge(25)
---
+++ Week-4/Day-1/Exercise-2/script.js
@@ -0,0 +1,8 @@
+
+/*-----------------Exercise-2------------------------*/
+function calcAge(myAge){
+    var mumAge = myAge*2;
+    return mumAge
+}
+
+console.log(calcAge(26))
