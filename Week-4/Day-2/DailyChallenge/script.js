728b08f1a573e5c232ec17b1dab8b20bcfe3c05b
OnlineElite <online.0.elite@gmail.com>
2023-05-04 19:14:51 +0100

    apdate week3/day-6

---
+++ Week-4/Day-2/DailyChallenge/script.js
@@ -0,0 +1,8 @@
+/*--------------------DailyChallenge---------------------*/
+
+var words = prompt("Enter a several words :")
+var  strArray = words.split(' ')
+
+ for(let i in strArray){
+    console.log(strArray[i])
+ }
