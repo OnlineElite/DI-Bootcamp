565cdef1b4c01e0d050c17570c2a83e2bc66b69e
OnlineElite <online.0.elite@gmail.com>
2023-05-30 20:40:35 +0100

    adding week5/day4/dailychallenge(Todo List)

--- Week-5/Day-4/DailyChallenge_TodoList/script.js
+++ Week-5/Day-4/DailyChallenge_TodoList/script.js
@@ -6,39 +6,91 @@ const inp = document.getElementById('inp')
 const list = document.getElementsByClassName('listTasks')[0]
 const submit = document.getElementById('submit')
 submit.addEventListener('click', addTask)
+
+// Add function
 function addTask(e){
     e.preventDefault()
     if(inp.value != ""){
         tasks.push(inp.value)
-        var xBtn = document.createElement('i')
-        xBtn.setAttribute("class", "fa-solid fa-square-xmark");
-        xBtn.style.color = "red"
-        list.appendChild(xBtn)
-        var checkBox = document.createElement('input')
-        checkBox.setAttribute('type', 'checkbox')
-        checkBox.style.marginInline = "5px"
-        checkBox.setAttribute('class', 'checked')
-        list.appendChild(checkBox)
-        var label = document.createElement('label')
-        label.textContent = inp.value;
-        label.style.fontSize = "18px"
-        list.appendChild(label)
-        var hr = document.createElement('hr')
-        hr.style.marginTop = "5px"
-        hr.style.marginBottom = "5px"
-        list.appendChild(hr)
+        let row = document.createElement('div')
+        row.setAttribute('class', 'rows')
+            var xBtn = document.createElement('i')
+            xBtn.setAttribute("class", "fa-solid fa-square-xmark");
+            xBtn.style.color = "red"
+            xBtn.setAttribute('data',inp.value)
+            row.appendChild(xBtn)
+
+            var checkBox = document.createElement('input')
+            checkBox.setAttribute('name', inp.value)
+            checkBox.setAttribute('type', 'checkbox')
+            checkBox.style.marginInline = "5px"
+            checkBox.setAttribute('class', 'chickes')
+            row.appendChild(checkBox)
+
+            var label = document.createElement('label')
+            label.textContent = inp.value;
+            label.setAttribute('name', inp.value)
+            label.style.fontSize = "18px"
+            row.setAttribute('data',inp.value)
+            row.appendChild(label)
+
+            var hr = document.createElement('hr')
+            hr.style.marginTop = "5px"
+            hr.style.marginBottom = "5px"
+            row.appendChild(hr)
+        list.appendChild(row)
+        ifElemChecked()
+        resetFunction()
     }
     else{
         alert('The empty field is empty, please enter a task')
     }
     inp.value = "";
 }
-
-let chackedElem = document.getElementsByClassName('checked')
-for(let i of chackedElem){
-    i.addEventListener('click',removeAction)
-    function removeAction(){
-        let lab = document.querySelector('i:first-child')
-        lab.style.textDecoration = "line-through"
+// Check function
+function ifElemChecked(){
+    let checkboxes = document.getElementsByClassName('chickes')
+    for(let i of checkboxes){
+        let iname =i.getAttribute('name')
+        i.addEventListener('change', function() {
+            if (this.checked) {
+                let labels = document.querySelectorAll('.listTasks div label')
+                for(let j of labels){
+                    let jname = j.getAttribute('name')
+                    if(iname === jname){
+                        j.classList.add('ChechedTask')
+                    }
+                    else{
+                        continue;
+                    }
+                }
+            }
+            else {
+                j.classList.add('notChechedTask')
+                
+            }
+        });
     }
+}
+//Reset function
+function resetFunction(){
+    var xButton = document.querySelectorAll('.listTasks div i')
+    var rows = document.querySelectorAll('.listTasks div')
+    for(let x of xButton){
+        let xdata = x.getAttribute('data')
+        x.addEventListener('click',removeTask)
+        function removeTask(e){
+            e.preventDefault();
+            for(let y of rows){
+                let ydata = y.getAttribute('data')
+                if(xdata === ydata){
+                    list.removeChild(y)
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+    }
+    
 }
