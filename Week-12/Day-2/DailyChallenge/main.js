46c5a24e61a9065939f5243099eeed4bfea6dcae
OnlineElite <online.0.elite@gmail.com>
2023-07-02 01:36:47 +0100

    adding week12/day2/dailychallenge

---
+++ Week-12/Day-2/DailyChallenge/main.js
@@ -0,0 +1,12 @@
+const largeNumber = 356;
+
+function getCurrentDate () {
+    return Date();
+};
+  
+  
+module.exports = {
+    myDate : getCurrentDate,
+    myVar : largeNumber
+}
+
---
+++ Week-12/Day-2/DailyChallenge/script.js
@@ -0,0 +1,32 @@
+let model = require('./main.js');
+const b = 5;
+console.log("result: "+ (model.myVar + b));
+//-----------------------------------------------
+
+const http = require('http')
+
+const server1 = http.createServer((req, res)=>{
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    const respose = `<html><body>
+                        <p>My Module is </p>
+                        <p>${(model.myVar + b)}</p>
+                        <h1>Hi there at the frontend </h1>
+                    </body></html>`;
+    res.end(respose);
+});
+server1.listen(3000,()=>{console.log(`I'm listening to server1...`);})
+
+//----------------------------------------------
+
+const server2 = http.createServer((req,res)=>{
+    console.log(model.myDate)
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    const respose = `<html><body>
+                        <p>Date and Time</p>
+                        <p>The date and time are currently: ${model.myDate()} in Morocco</p>
+                    </body></html>`;
+    res.end(respose);
+})
+server2.listen(8080,()=>{console.log(`I'm listening to server 2...`);})
