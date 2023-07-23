35c85c1bb9c2bac7bfe01c7402a18a998917263f
OnlineElite <online.0.elite@gmail.com>
2023-07-23 19:55:11 +0100

    updating week16/day5/dailychallenge

--- Week-16/Day-2/DailyChallenge/movies/src/components/MoviesList.js
+++ Week-16/Day-2/DailyChallenge/movies/src/components/MoviesList.js
@@ -4,25 +4,22 @@ import { selectMovie } from '../actions';
 import '../Movie.css'
 
 const MovieList = ({ movies, selectMovie }) => {
-  const renderMovies = () => {
-    return(
-      <div className='list'>
-        <h2>Movies List</h2>
-        <div className='box'>
-            <ul>
-                {movies.map((movie)=>(
-                    <div key={movie.title} className='row'>
-                        <li> {movie.title} </li>
-                        <button onClick={() => selectMovie(movie)}>details</button> 
-                    </div>
-                ))}
-            </ul>
-        </div>
-    </div>
-    ) 
-  };
 
-  return <div>{renderMovies()}</div>;
+  return(
+    <div className='list'>
+      <h2>Movies List</h2>
+      <div className='box'>
+          <ul>
+              {movies.map((movie)=>(
+                  <div key={movie.title} className='row'>
+                      <li> {movie.title} </li>
+                      <button onClick={() => selectMovie(movie)}>details</button> 
+                  </div>
+              ))}
+          </ul>
+      </div>
+    </div>
+  ) 
 };
 
 const mapStateToProps = (state) => {
